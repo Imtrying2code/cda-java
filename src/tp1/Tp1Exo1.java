cd69fbafe8d9502cd99fca2f9786bb09ff180357
FPOM <44132278+Imtrying2code@users.noreply.github.com>
2019-11-04 15:31:04 +0100

    Push Tp1 et Tp2

--- src/tp1/Tp1Exo1.java
+++ src/tp1/Tp1Exo1.java
@@ -1,9 +1,26 @@
 package tp1;
 
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Tp1Exo1 {
 
 	public static void main(String[] args) {
-		System.out.print("TP1");
+		Scanner user = new Scanner(System.in);
+				
+		
+		int valeur_1;
+		System.out.print("Saisir un entier : ");
+		valeur_1 = user.nextInt();
+		int valeur_2;
+		System.out.print("Saisir un entier : ");
+		valeur_2 = user.nextInt();
+		
+		
+		System.out.print("Somme des deux valeurs : "+ (valeur_1+valeur_2)+ " Produit des deux valeurs : "+ (valeur_1 * valeur_2));
+		
+		user.close();
 	}
 
 }
