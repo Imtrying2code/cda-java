fd4ae41a4c4b02ec5e80881c8b09e8797732b5cb
FPOM <44132278+Imtrying2code@users.noreply.github.com>
2019-11-06 11:07:14 +0100

    Finish TP3Exo 1,2,3

---
+++ src/tp3/Tp3Exo2.java
@@ -0,0 +1,23 @@
+package tp3;
+
+public class Tp3Exo2 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+   
+		int n = 10, p = 4;   
+		long q = 2111000222 ;   
+		float x = 1.75f;
+		 
+		  System.out.println("n+q('long') = " + (n+q));
+		  System.out.println("n+x('float') = " + (n+x));
+		  System.out.println("n%p*q('long') = " + (n%p*q));
+		  System.out.println("n%-p*n('long') = " + (n%-p*n));
+		  System.out.println("-n%p*n('long') = " + (-n%p*n));
+	}
+}
+	
+
+
