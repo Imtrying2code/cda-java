b9e91d0687117fe81491e01f299e0221fb9c23b0
Franck POMPEANI <franck.pompeani@gmail.com>
2019-11-15 06:55:47 +0100

    Mise à jour Linux

--- src/tp4/Tp4Ex05.java
+++ src/tp4/Tp4Ex05.java
@@ -1,4 +1,4 @@
-/*Nom du répertoire : TP4
+/*Nom du rï¿½pertoire : TP4
 Nom du projet : Tp4Exo5
 Nom de la classe : Tp4Exo5
 Auteur : Franck */
@@ -14,7 +14,7 @@ public class Tp4Exo5 {
 		int tentative = 0;
 		Scanner clavier = new Scanner(System.in);
 		
-		System.out.println("Bonjour! Quel est le code d'accès?");
+		System.out.println("Bonjour! Quel est le code d'accï¿½s?");
 		int n = clavier.nextInt();
 		tentative++;
 		while (tentative<5) {
@@ -23,14 +23,14 @@ public class Tp4Exo5 {
 				break;
 			}
 			else {
-				System.out.println("Code erroné! Il vous reste "+(5-tentative)+" tentative(s)");
-				System.out.println("Quel est le code d'accès?");
+				System.out.println("Code erronï¿½! Il vous reste "+(5-tentative)+" tentative(s)");
+				System.out.println("Quel est le code d'accï¿½s?");
 				n = clavier.nextInt();
 			}
 			tentative++;
 		}
 		if (tentative==5) {
-			System.out.println("Essais incorrects trop nombreux, veuillez contacter le service client pour réinitialiser le code d'accès :)");
+			System.out.println("Essais incorrects trop nombreux, veuillez contacter le service client pour rï¿½initialiser le code d'accï¿½s :)");
 		}
 		clavier.close();
 	}
